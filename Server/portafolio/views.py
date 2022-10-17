d30db61631f15c4c1f4219923106817a8a854592
Estuardo Ramírez <estuardito_9@hotmail.com>
2022-10-17 00:28:06 -0600

    Se mejoro el obtener ip

--- Server/portafolio/views.py
+++ Server/portafolio/views.py
@@ -8,7 +8,16 @@ from ipware import get_client_ip
 # IndexView ("estuardodev.com" | "www.estuardodev.com")
 def IndexView(request):
     template_name: str = "portafolio/index.html"
-    ip_client, ip_boolean = get_client_ip(request)
+    
+    client_ip, is_routable = get_client_ip(request)
+    if client_ip is None:
+        ip_client = 'NO SE HA LOGRADO CAPTURAR TU IP'
+    else:
+        ip_client = client_ip
+    if is_routable:
+        ip_client = client_ip
+    else:
+        ip_client = client_ip
 
     return render(request, template_name, {'ip_client' : ip_client})
     
