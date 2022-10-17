4bdb0bff5edf3ceb41bf8dfc6844072d25f825e3
Estuardo Ramírez <estuardito_9@hotmail.com>
2022-10-17 01:26:16 -0600

    Se agrego /terceros a subdominio

--- Server/cripto/urls.py
+++ Server/cripto/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from . import views
+from portafolio.views import AtributionView
 
 app_name = 'criptosara'
 
 urlpatterns = [
     path('', views.SaraIndexView.as_view(), name="saraindex"),
+    path('terceros/', AtributionView.as_view(), name="atribucion")
 ]
