fb6b988f3c1c58c7dbf5af1a37e45627f7886bf7
Estuardo Ramírez <estuardito_9@hotmail.com>
2022-10-17 16:42:55 -0600

    Se agrego el error 404 a subdominio

--- Server/cripto/urls.py
+++ Server/cripto/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from django.conf.urls import handler404
 
 from . import views
-from portafolio.views import AtributionView
+from portafolio.views import AtributionView, Error404View
 
 app_name = 'criptosara'
 
@@ -9,3 +10,5 @@ urlpatterns = [
     path('', views.SaraIndexView.as_view(), name="saraindex"),
     path('terceros/', AtributionView.as_view(), name="atribucion")
 ]
+
+handler404 = Error404View.as_view()
