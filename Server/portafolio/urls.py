9b8e1003a5622a9a1dc34a18f65beff2086cc1eb
Estuardo Ramírez <estuardito_9@hotmail.com>
2022-10-16 21:38:04 -0600

    Se captura la ip del usuario y se dice en el index

--- Server/portafolio/urls.py
+++ Server/portafolio/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'portafolio'
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name="index")
+    path('', views.IndexView, name="index")
 ]
--- Server/portafolio/views.py
+++ Server/portafolio/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
+from ipware import get_client_ip
+
+
 
 # IndexView ("estuardodev.com" | "www.estuardodev.com")
-class IndexView(generic.TemplateView):
+def IndexView(request):
     template_name: str = "portafolio/index.html"
+    ip_client, ip_boolean = get_client_ip(request)
+
+    return render(request, template_name, {'ip_client' : ip_client})
+    
+    
 
 class Error404View(generic.TemplateView):
     template_name: str = "error/404.html"
