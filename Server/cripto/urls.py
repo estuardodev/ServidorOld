0fbaa2f90da338c3c510ca4496dd39f77e48cd97
Estuardo Ramírez <estuardito_9@hotmail.com>
2022-10-19 18:04:14 -0600

    Se cambio la forma de mostrar la template de robots.txt

--- Server/cripto/urls.py
+++ Server/cripto/urls.py
@@ -9,7 +9,7 @@ app_name = 'criptosara'
 
 urlpatterns = [
     path('', views.SaraIndexView.as_view(), name="saraindex"),
-    path('robots.txt', TemplateView.as_view(template_name="cripto/robots.txt", content_type="text/html")),
+    path('robots.txt', views.RobotsView.as_view()),
     path('sitemap.xml', TemplateView.as_view(template_name="portafolio/sitemap.xml", content_type="text/xml")),
     path('BingSiteAuth.xml', TemplateView.as_view(template_name="portafolio/BingSiteAuth.xml", content_type="text/xml")),
     path('terceros/', AtributionView.as_view(), name="atribucion"),
--- Server/cripto/views.py
+++ Server/cripto/views.py
@@ -5,6 +5,10 @@ from django.views import generic
 class SaraIndexView(generic.TemplateView):
     template_name: str = "cripto/sara.html"
 
+class RobotsView(generic.TemplateView):
+    template_name: str = "cripto/robots.txt"
+    content_type = 'text/plain'
+
 class ErrorCripto404Views(generic.TemplateView):
     template_name: str = "cripto/error/404/404.html"
 
