9a38cc160fce54817b59cab1d5c256766adfd462
Estuardo Ramírez <estuardito_9@hotmail.com>
2022-12-15 13:08:28 -0600

    Se creo la estructura base y las url de seo y ads

--- Server/blog/urls.py
+++ Server/blog/urls.py
@@ -1,6 +1,23 @@
+# DJANGO
 from django.urls import path
+from django.views.generic.base import TemplateView
+from django.conf.urls import handler404, handler500
+
+# MODULO PROPIO
 from . import views
 
 urlpatterns = [
-    path('', views.indexView, name='IndexView')
+    # SITIO
+    path('', views.indexView, name='IndexView'),
+
+    # SEO
+    path('robots.txt', TemplateView.as_view(template_name="blog/robots.txt", content_type="text/plain")),
+    path('ads.txt', TemplateView.as_view(template_name="blog/ads.txt", content_type="text/plain")),
+    path('sitemap.xml', TemplateView.as_view(template_name="blog/sitemap.xml", content_type="text/xml")),
+    #path('BingSiteAuth.xml', TemplateView.as_view(template_name="portafolio/BingSiteAuth.xml", content_type="text/xml")),
 ]
+
+
+# MANEJO DE ERRORES HTTP
+handler404 = views.Error404.as_view() # Error 404
+handler500 = views.Error500.as_view() # Error 500
--- Server/blog/views.py
+++ Server/blog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse # Respuesta HTTP
+from django.views import generic
 
 # Create your views here.
 def indexView(request):
-    return HttpResponse('HOLA MUNDO')
+    return render(request, 'blog/index.html')
+
+class Error404(generic.TemplateView):
+    template_name: str = "error/404/404.html"
+
+class Error500(generic.TemplateView):
+    template_name: str = "error/500/500.html"
