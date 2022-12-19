019de1637bad991a86c0772c589dfc19d9fd25a9
Estuardo Ramírez <estuardito_9@hotmail.com>
2022-12-19 00:01:27 -0600

    Prueba 6

--- Server/estuardodev/hosts.py
+++ Server/estuardodev/hosts.py
@@ -2,10 +2,6 @@ from django.conf import settings
 
 from django_hosts import patterns, host
 
-from django.conf.urls import handler404, handler500
-
-from . import views
-
 
 host_patterns = patterns(
     '',
@@ -13,7 +9,3 @@ host_patterns = patterns(
     host(r'blog', 'blog.urls', name="blog"),
 
 )
-
-# MANEJO DE ERRORES HTTP
-handler404 = views.Error404 # Error 404
-handler500 = views.Error500 # Error 500
