2c358e26587d7a28b7db04ebbf958c52a2c93f66
Estuardo Ramirez <estuardito_9@hotmail.com>
2022-12-20 13:15:07 -0600

    Se mejoro el administrador

--- Server/blog/admin.py
+++ Server/blog/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
 from . import models
+
+# Personalizacion de Administrador
+admin.site.site_header = 'estuardodev'
+admin.site.index_title = 'Panel de control de mi sitio'
+admin.site.site_title = 'Administrador'
 # Register your models here.
-admin.site.register(models.Articulo)
+
+
+class ArticuloAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'creado')
+
+    search_fields = ('titulo', 'creado')
+    fields = ('titulo', 'description', 'tags', 'url', 'prioridad', 'contenido', 'imagen', 'alt_imagen')
+
+admin.site.register(models.Articulo, ArticuloAdmin)
