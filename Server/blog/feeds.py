86afd04e62f99db2f80c9551028cd94e7ae9e2c7
Estuardo Ramirez <estuardito_9@hotmail.com>
2023-01-04 18:29:00 -0600

    Intenta solucionar el problema de imagenes del feed

--- Server/blog/feeds.py
+++ Server/blog/feeds.py
@@ -30,7 +30,9 @@ class UltimasNoticias(Feed):
     def item_enclosure_mime_type(self, item):
         return "image/webp"
 
-    def item_enclosure_url(self, item):
-        return f'https://blog.estuardodev.com/media/{item.imagen}'
-
-    
+    def item_enclosure_length(self, item):
+        try:
+            response = urllib.request.urlopen(f'https://blog.estuardodev.com/media/{item.image_url}')
+            return int(response.info()["Content-Length"])
+        except urllib.error.HTTPError:
+            return 0
