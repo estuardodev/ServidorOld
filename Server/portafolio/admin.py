47e80b1b6e5c993854b8e3a9a9ef4a81617cc845
Estuardo Ramirez <estuardito_9@hotmail.com>
2023-01-06 19:07:16 -0600

    Se mejoro el admin de portafolio

--- Server/portafolio/admin.py
+++ Server/portafolio/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import IPClient, IPClientVisitas
 
 # Register your models here.
+
+class IPBlog(admin.ModelAdmin):
+    list_display = ('id','ip_add')
+    search_fields = ('id', 'ip_add')
+    readonly_fields = ('id','ip_add')
+
+admin.site.register(model_or_iterable=(IPClient), admin_class=IPBlog)
+
+class VisitasBLog(admin.ModelAdmin):
+    list_display = ('ip_key', 'visitas')
+    search_fields = ('ip_key','visitas', 'id')
+    readonly_fields = ('id', 'ip_key', 'visitas')
+
+admin.site.register(model_or_iterable=(IPClientVisitas), admin_class=VisitasBLog)
