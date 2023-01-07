a740443cdf13237a65d332870f40345b1e5ad00e
Estuardo Ramirez <estuardito_9@hotmail.com>
2023-01-06 19:08:52 -0600

    Se mejoro algunas cosas del admin

--- Server/portafolio/models.py
+++ Server/portafolio/models.py
@@ -7,6 +7,14 @@ class IPClient(models.Model):
     def __str__(self):
         return self.ip_add
 
+    class Meta:
+        verbose_name = "IP CLientes"
+        verbose_name_plural = "IP Clientes"
+
 class IPClientVisitas(models.Model):
     ip_key = models.ForeignKey(IPClient, on_delete=models.CASCADE)
     visitas = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name = "Visitas de IP"
+        verbose_name_plural = "Visitas de IP"
