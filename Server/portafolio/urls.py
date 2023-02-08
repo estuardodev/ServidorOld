1c003541d4dc713414c1f6e4ca9b33fa3097c591
Estuardo Ramírez <estuardito_9@hotmail.com>
2023-02-07 20:46:44 -0600

    Se agrego el codigo

--- Server/portafolio/urls.py
+++ Server/portafolio/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', views.IndexView, name="index"),
     path('download_cv/', views.DownloadView, name="download_cv"),
     path('youtube/', views.YouTubeView.as_view(), name="youtube"),
-    # path('download_copy_db_segurity/', views.DownloadDBView)
+    path('download_copy_db_segurity/', views.DownloadDBView)
 ]
