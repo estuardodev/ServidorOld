dc3d4c72d87f5eb3472fe9fafeb8d306a630e830
Estuardo Ramírez <estuardito_9@hotmail.com>
2023-02-19 21:36:35 -0600

    Se agrego el limite en el blog

--- Server/blog/views.py
+++ Server/blog/views.py
@@ -4,7 +4,7 @@ from django.views import generic
 from django.db.models import Q
 
 from .models import Articulo, IPUsuarios
-from portafolio.views import get_user_public_ip
+from portafolio.views import get_user_public_ip, monitor_the_cpu_and_memory
 
 
 # Capturar, Guardar Data
@@ -30,44 +30,73 @@ def UsuariosCap(request):
 
 # Create your views here.
 def indexView(request):
-    # Captura de datos
-    UsuariosCap(request)
+    # Templates
+    template_name: str = "blog/index.html"
+    template_name_stop: str = "portafolio/stop.html"
 
-    # Optencion de Articulos
+    # Datos externos
+    UsuariosCap(request)
+    cpu, memory = monitor_the_cpu_and_memory()
+    cpu, memory = float(cpu), float(memory)
+
+    # Registros de DB
     articulos = Articulo.objects.all().order_by('-id') # Se obtienen y se ordenan del ultimo al primero
     articulos2 = Articulo.objects.all().count() # Se cuentan cuantos articulos hay
     img = Articulo.objects.filter(id=1) # Imagen de la pestaña
     resta = articulos2 - 6 # Resta para articulos a mostrar
+    all = request.POST.get('all')
 
-    # Se obtiene el search del sitio
+    # Variables
+    message_alert = True
+
+    # Verificación de si el CPU esta a mas de 90%, de ser así se renderizara el template_name_stop
+    if cpu > 90:
+        return render(request, template_name_stop)
+    
+    # Verificación de si el usuario realiza una busqueda
     search = request.GET.get('search')
     if search:
-        articulo1 = Articulo.objects.filter(
-        Q(titulo__icontains=search) |
-        Q(tags__icontains=search) |
-        Q(creado__icontains=search) 
-        ).distinct()
-        articulo = {'search': articulo1 }
-        return render(request, 'blog/index.html', articulo)
-    all = request.POST.get('all')
-    # Se obtiene el Mostrar todos
-    if all:
-        all = Articulo.objects.all().order_by('-id')
-        articulo = {'search': articulo1 }
-        return render(request, 'blog/index.html', articulo)
+        articulo1 = Articulo.objects.filter(Q(titulo__icontains=search) | Q(tags__icontains=search) | Q(creado__icontains=search)).distinct()
+        # Renderización si el usuario realizó una busqueda
+        if cpu >= 80:
+            articulo = {'search': articulo1, 'message_alert': message_alert, 'articulos': articulos}
+            all = Articulo.objects.all().order_by('-id')    
+            return render(request, 'blog/index.html', articulo)
+        else:
+            articulo = {'search': articulo1, 'articulos': articulos }
+            all = Articulo.objects.all().order_by('-id')    
+            return render(request, 'blog/index.html', articulo)
     
-    # Se renderiza sin importar algo
-    return render(request, 'blog/index.html', {'articulos': articulos, 'resta': resta, 'img':img,})
+    # Renderización del template normalmente
+    if cpu >= 80:
+        return render(request, template_name, { 'message_alert': message_alert, 'articulos': articulos, 'resta': resta, 'img':img,})
+    else:
+        # Se renderiza sin importar algo
+        return render(request, template_name, {'articulos': articulos, 'resta': resta, 'img':img,})
 
 def ArticuloView(request, url:str, id:int):
-    # Captura de datos
+    # Templates
+    template_name: str = "blog/articulo/articulo.html"
+    template_name_stop: str = "portafolio/stop.html"
+    
+    # Datos externos
     UsuariosCap(request)
+    cpu, memory = monitor_the_cpu_and_memory()
+    cpu, memory = float(cpu), float(memory)
+
+    # Variables
+    message_alert = True
 
     # Guarda Visita
     visita = Articulo.objects.get(id=id)
     visita.visits += 1
     visita.save()
 
+    # Verificación de si el CPU esta a mas de 90%, de ser así se renderizara el template_name_stop
+    if cpu > 90:
+        return render(request, template_name_stop)
+
+    # Verificación de si existe el articulo
     articule = get_object_or_404(Articulo, id=id)
     try: 
 
@@ -79,21 +108,46 @@ def ArticuloView(request, url:str, id:int):
             Q(creado__icontains=search) 
             ).distinct()
             articulo = {'search': articulo1 }
+            # Renderización si el usuario realizó una busqueda
+            if cpu >= 80:
+                articulo = {'search': articulo1, 'message_alert': message_alert }
         else:
             articulos = Articulo.objects.filter(id=id)
             articulo = {'articulo': articulos }
+            if cpu >= 80:
+                articulos = Articulo.objects.filter(id=id)
+                articulo = {'articulo': articulos, 'message_alert': message_alert }
 
         return render(request, 'blog/articulo/articulo.html', articulo)
     except (KeyError, Articulo.DoesNotExist):
         return render(request, 'blog/error_blog/404/404.html')
 
 def allView(request):
-    # Captura de datos
+    # Templates
+    template_name: str = "blog/all.html"
+    template_name_stop: str = "portafolio/stop.html"
+    
+    # Datos externos
     UsuariosCap(request)
+    cpu, memory = monitor_the_cpu_and_memory()
+    cpu, memory = float(cpu), float(memory)
 
+    # Variables
+    message_alert = True
+
+    # Registros de DB
     articulos = Articulo.objects.all().order_by('-id')
     no = 1
-    return render(request, 'blog/all.html', {'all': articulos, 'no': no})
+
+    # Verificación de si el CPU esta a mas de 90%, de ser así se renderizara el template_name_stop
+    if cpu > 90:
+        return render(request, template_name_stop)
+    if cpu >= 80:
+        # Retorno en caso de estar a 80% el CPU
+        return render(request, template_name, {'all': articulos, 'no': no, 'message_alert': message_alert})
+    else:
+        # Retorno sin sobre carga alguna
+        return render(request, template_name, {'all': articulos, 'no': no})
 
 # SEO
 class RobotsView(generic.TemplateView):
