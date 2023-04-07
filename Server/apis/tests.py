53f22c944c8b97424ffefe29b0cb1358d164027f
Estuardo Ramírez <estuardito_9@hotmail.com>
2023-04-06 22:15:38 -0600

    Se agregaron los test de la api de get-ip-users

--- Server/apis/tests.py
+++ Server/apis/tests.py
@@ -20,3 +20,13 @@ class TestingOfTheApis(TestCase):
         '''Probaremos si obtenemos un 404 al solicitar un articulo que no existe a mi API'''
         response = self.client.get(reverse("myApis:apiArticulo", args=[10]))
         self.assertEqual(response.status_code, 404)
+
+    def testGetCorrectInfoIP(self):
+        '''Probaremos obtener la información de una IP que si existe (8.8.8.8) de Google'''
+        response = self.client.get(reverse("myApis:apiIPUsers", args=["8.8.8.8"]))
+        self.assertContains(response, "Success")
+
+    def testGetIncorrectInfoIP(self):
+        '''Probaremos obtener la información de una IP que no existe (222.20.1.) Inexistente por el punto'''
+        response = self.client.get(reverse("myApis:apiIPUsers", args=["222.20.1."]))
+        self.assertContains(response, "Fail")
