a3023d4fd975c24dad1001d01d02f62bb19033a8
Estuardo Ramírez <estuardito_9@hotmail.com>
2023-04-06 21:54:19 -0600

    Se mejoro la documentacion de mi API

--- Server/apis/views.py
+++ Server/apis/views.py
@@ -11,7 +11,11 @@ from blog.models import Articulo
 
 # Create your views here.
 def indexView(request):
-    response = {"message":"Success"}
+    response = {
+        "message":"Success, these are the available APIs",
+        "getArticulo":"With this API you get information about any of my articles with the id you request. URL: estuardodev.com/api/articulo/<id>",
+        "getIPUsers":"With this API you get information about the IP address you designate in the url. URL: estuardodev.com/api/ip-users/<ip>"
+        }
     return JsonResponse(response)
 
 # API que permite obtener un articulo de mi blog
@@ -26,12 +30,20 @@ def getArticulo(request, pk:int):
 
 # API que permite obtener informacion de los usuarios acerca de su IP
 def getIPUsers(request, ip:str):
-    try:
-        petition = requests.get(f"http://ip-api.com/json/{ip}")
-        petition = petition.json()
+    # Realizamos una peticion al servicio de http://ip-api.com
+    petition = requests.get(f"http://ip-api.com/json/{ip}")
+    petition = petition.json()
+    
+    # Realizamos la validación de la información que nos devuelve la petición
+    # De ser success entonces lo acoplamos a nuestra forma y eliminamos su mensaje
+    if petition["status"] == "success":
+        del petition["status"]
         information = {'message': 'Success', 'information': petition}
-    except Articulo.DoesNotExist:
-        information = {'message': 'Not Found'}
+    elif petition["status"] == "fail": # De ser fail, lo acoplamos a nuestra forma y eliminamos su mensaje
+        del petition["status"]
+        information = {'message': 'Fail', 'information': petition}
+    else: # De no cumplirse por si dado caso cambien los mensajes, entonces mostrara la informacion normal sin alterar
+        information = {'message': 'Success', 'information': petition}    
 
     return JsonResponse(information)
 
