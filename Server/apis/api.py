25f7e6fbc4c6dbc861678ae9a65b2a032a4f0d81
Estuardo Ramírez <estuardito_9@hotmail.com>
2023-04-08 15:00:16 -0600

    Se quito la interfaz de django rest framework para la api articulo

--- Server/apis/api.py
+++ Server/apis/api.py
@@ -1,9 +1,10 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, renderers
 from blog.models import Articulo
 from .serializers import ProjectSerializers
 
 class ProjectViewSet(viewsets.ModelViewSet):
     http_method_names = ['get']
+    renderer_classes = [renderers.JSONRenderer]
     queryset = Articulo.objects.all().order_by("-id")
     permission_classes = [permissions.AllowAny]
     serializer_class = ProjectSerializers
